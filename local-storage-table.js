2384952ab2356866b98ce332cb49a265bd1a620b
knziiy <knock@cid4sm.com>
2023-04-01 19:26:11 +0900

    Search results now appear with the matching strings highlighted

--- local-storage-table.js
+++ local-storage-table.js
@@ -1,192 +1,221 @@
 const app = Vue.createApp({
-    data() {
-      return {
-        headers: [],
-        rows: [],
-        searchText: "",
+  data() {
+    return {
+      headers: [],
+      rows: [],
+      searchText: "",
 
-        // Add this data property for datasets
-        datasets: [
-          {
-            name: "None",
-            headers: [],
-            rows: [],
-          },
-        ],
-        selectedDatasetIndex: 0, // default selected dataset
-      };
-    },
-    computed: {
-      filteredRows() {
-        if (!this.searchText) {
-          return this.rows;
-        }
-        const searchWords = this.searchText.toLowerCase().split(/\s+/);
-        return this.rows.filter((row) => {
-          return searchWords.every((word) =>
+      // Add this data property for datasets
+      datasets: [
+        {
+          name: "None",
+          headers: [],
+          rows: [],
+        },
+      ],
+      selectedDatasetIndex: 0, // default selected dataset
+    };
+  },
+  computed: {
+    filteredRows() {
+      const searchWordsAll = this.searchText.toLowerCase().split(/\s+/);
+      const searchWords = searchWordsAll.filter((word) => word.length > 0);
+
+      const escapedRows = this.rows.map((row) =>
+        row.map((cell) => this.escapeHtml(cell))
+      );
+
+      if (!this.searchText) {
+        return escapedRows;
+      }
+
+      return escapedRows
+        .filter((row) =>
+          searchWords.every((word) =>
             row.some((cell) => cell.toLowerCase().includes(word))
-          );
-        });
-      },
-      selectedDataset() {
-        return this.datasets[this.selectedDatasetIndex];
-      },
+          )
+        )
+        .map((row) => row.map((cell) => this.highlighted(cell, searchWords)));
     },
-    methods: {
-      async loadCSV(event) {
-        const file = event.target.files[0];
-        if (!file) {
-          return;
-        }
+    selectedDataset() {
+      return this.datasets[this.selectedDatasetIndex];
+    },
+  },
+  methods: {
+    async loadCSV(event) {
+      const file = event.target.files[0];
+      if (!file) {
+        return;
+      }
 
-        const reader = new FileReader();
-        reader.onload = (e) => {
-          const content = e.target.result;
-          this.processCSV(content, file.name);
-        };
-        reader.readAsBinaryString(file);
-      },
-      processCSV(content, datasetName) {
-        const encodingType = Encoding.detect(content);
-        const content_utf = Encoding.convert(content, {
-          from: encodingType,
-          to: "UNICODE",
-          type: "string",
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        const content = e.target.result;
+        this.processCSV(content, file.name);
+      };
+      reader.readAsBinaryString(file);
+    },
+    processCSV(content, datasetName) {
+      const encodingType = Encoding.detect(content);
+      const content_utf = Encoding.convert(content, {
+        from: encodingType,
+        to: "UNICODE",
+        type: "string",
+      });
+
+      const rows = this.parseCSVRows(content_utf);
+
+      this.headers = rows.shift();
+      this.rows = rows;
+
+      // Save the dataset to LocalStorage
+      if (!datasetName) {
+        const inputDatasetName = prompt(
+          "Enter a name for the dataset",
+          "Dataset"
+        );
+        datasetName = inputDatasetName;
+      }
+      if (datasetName) {
+        this.datasets.push({
+          name: datasetName,
+          headers: this.headers,
+          rows: this.rows,
         });
+        localStorage.setItem("datasets", JSON.stringify(this.datasets));
+      }
+    },
+    parseCSVRows(text) {
+      const lines = text.trim().split(/\r?\n/);
+      const rows = [];
 
-        const rows = this.parseCSVRows(content_utf);
+      let currentRow = [];
+      let currentValue = "";
+      let inQuotes = false;
 
-        this.headers = rows.shift();
-        this.rows = rows;
-
-        // Save the dataset to LocalStorage
-        if (!datasetName) {
-          const inputDatasetName = prompt(
-            "Enter a name for the dataset",
-            "Dataset"
-          );
-          datasetName = inputDatasetName;
-        }
-        if (datasetName) {
-          this.datasets.push({
-            name: datasetName,
-            headers: this.headers,
-            rows: this.rows,
-          });
-          localStorage.setItem("datasets", JSON.stringify(this.datasets));
-        }
-      },
-      parseCSVRows(text) {
-        const lines = text.trim().split(/\r?\n/);
-        const rows = [];
-
-        let currentRow = [];
-        let currentValue = "";
-        let inQuotes = false;
-
-        for (const line of lines) {
-          for (let i = 0; i < line.length; i++) {
-            const currentChar = line[i];
-
-            if (inQuotes) {
-              if (currentChar === '"') {
-                if (line[i + 1] === '"') {
-                  currentValue += '"';
-                  i++;
-                } else {
-                  inQuotes = false;
-                }
-              } else {
-                currentValue += currentChar;
-              }
-            } else {
-              if (currentChar === '"') {
-                inQuotes = true;
-              } else if (currentChar === ",") {
-                currentRow.push(currentValue);
-                currentValue = "";
-              } else {
-                currentValue += currentChar;
-              }
-            }
-          }
+      for (const line of lines) {
+        for (let i = 0; i < line.length; i++) {
+          const currentChar = line[i];
 
           if (inQuotes) {
-            currentValue += "\n";
+            if (currentChar === '"') {
+              if (line[i + 1] === '"') {
+                currentValue += '"';
+                i++;
+              } else {
+                inQuotes = false;
+              }
+            } else {
+              currentValue += currentChar;
+            }
           } else {
-            currentRow.push(currentValue);
-            rows.push(currentRow);
-            currentRow = [];
-            currentValue = "";
+            if (currentChar === '"') {
+              inQuotes = true;
+            } else if (currentChar === ",") {
+              currentRow.push(currentValue);
+              currentValue = "";
+            } else {
+              currentValue += currentChar;
+            }
           }
         }
 
-        return rows;
-      },
-      clearLocalStorage() {
-        localStorage.removeItem("datasets");
-        this.datasets = [
-          {
-            name: "Default",
-            headers: [],
-            rows: [],
-          },
-        ];
-        this.selectedDatasetIndex = 0;
-      },
-      async loadInitialData() {
-        // Load data from LocalStorage
-        const datasets = localStorage.getItem("datasets");
-
-        if (datasets) {
-          this.datasets = JSON.parse(datasets);
-          return;
+        if (inQuotes) {
+          currentValue += "\n";
+        } else {
+          currentRow.push(currentValue);
+          rows.push(currentRow);
+          currentRow = [];
+          currentValue = "";
         }
+      }
 
-        // Fetch initial data if not in LocalStorage
-        try {
-          const response = await fetch("initialdb.csv");
-          if (!response.ok) {
-            throw new Error(`HTTP error: ${response.status}`);
-          }
-
-          const content = await response.text();
-          this.processCSV(content, "Sample Data");
-        } catch (error) {
-          console.error("Failed to load initial data:", error);
-        }
-      },
-      selectDataset(index) {
-        this.selectedDatasetIndex = index;
-        this.headers = this.selectedDataset.headers;
-        this.rows = this.selectedDataset.rows;
-      },
-      handleDragEnter(event) {
-        console.log("Drag Enter");
-        event.preventDefault();
-        event.dataTransfer.dropEffect = "copy";
-      },
-      handleDragOver(event) {
-        console.log("Drag Over");
-        event.preventDefault();
-        event.dataTransfer.dropEffect = "copy";
-      },
-      handleDragLeave(event) {
-        console.log("Drag Leave");
-        event.preventDefault();
-      },
-      handleDrop(event) {
-        console.log("Dropped File");
-        event.preventDefault();
-        const file = event.dataTransfer.files[0];
-        if (!file) {
-          return;
-        }
-        this.loadCSV({ target: { files: [file] } });
-      },
+      return rows;
     },
-    created() {
-      this.loadInitialData();
+    clearLocalStorage() {
+      localStorage.removeItem("datasets");
+      this.datasets = [
+        {
+          name: "Default",
+          headers: [],
+          rows: [],
+        },
+      ];
+      this.selectedDatasetIndex = 0;
     },
-  });
-  app.mount("#app");
+    async loadInitialData() {
+      // Load data from LocalStorage
+      const datasets = localStorage.getItem("datasets");
+
+      if (datasets) {
+        this.datasets = JSON.parse(datasets);
+        return;
+      }
+
+      // Fetch initial data if not in LocalStorage
+      try {
+        const response = await fetch("initialdb.csv");
+        if (!response.ok) {
+          throw new Error(`HTTP error: ${response.status}`);
+        }
+
+        const content = await response.text();
+        this.processCSV(content, "Sample Data");
+      } catch (error) {
+        console.error("Failed to load initial data:", error);
+      }
+    },
+    selectDataset(index) {
+      this.selectedDatasetIndex = index;
+      this.headers = this.selectedDataset.headers;
+      this.rows = this.selectedDataset.rows;
+    },
+    handleDragEnter(event) {
+      console.log("Drag Enter");
+      event.preventDefault();
+      event.dataTransfer.dropEffect = "copy";
+    },
+    handleDragOver(event) {
+      console.log("Drag Over");
+      event.preventDefault();
+      event.dataTransfer.dropEffect = "copy";
+    },
+    handleDragLeave(event) {
+      console.log("Drag Leave");
+      event.preventDefault();
+    },
+    handleDrop(event) {
+      console.log("Dropped File");
+      event.preventDefault();
+      const file = event.dataTransfer.files[0];
+      if (!file) {
+        return;
+      }
+      this.loadCSV({ target: { files: [file] } });
+    },
+    escapeHtml(text) {
+      const div = document.createElement("div");
+      div.textContent = text;
+      return div.innerHTML;
+    },
+    highlighted(cell, searchWords) {
+      const escapedSearchWords = searchWords.map((word) =>
+        this.escapeHtml(word)
+      );
+
+      const newCell = escapedSearchWords.reduce((acc, word) => {
+        const regex = new RegExp(`(${word})(?![^<]*>|[^<>]*<\\/)`, "gi");
+        return acc.replace(
+          regex,
+          (match) => `<span class='highlighted'>${match}</span>`
+        );
+      }, cell);
+
+      return newCell;
+    },
+  },
+  created() {
+    this.loadInitialData();
+  },
+});
+app.mount("#app");
